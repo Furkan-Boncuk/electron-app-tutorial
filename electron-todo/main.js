627605701e92d3bcc3f4b21161d59b8fd038671e
Furkan-Boncuk <furkanboncuk2000@gmail.com>
2024-09-08 18:56:14 +0300

    eski kodlar electron-basics klasörü altına alındı ve electron-todo klasörü altında todo list projesinin kurulumu yapıldı.

---
+++ electron-todo/main.js
@@ -0,0 +1,96 @@
+const electron = require("electron");
+const url = require("url");
+const path = require("path");
+
+const { app, BrowserWindow, Menu, ipcMain } = electron;
+
+let mainWindow;
+
+app.on("ready", () => {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+    movable: true,
+  });
+
+  mainWindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname, "pages/mainWindow/mainWindow.html"),
+      protocol: "file:",
+      slashes: true,
+    })
+  );
+
+  const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+
+  Menu.setApplicationMenu(mainMenu);
+});
+
+const mainMenuTemplate = [
+  {
+    label: "Dosya",
+    submenu: [
+      {
+        label: "Add New TODO",
+        click: () => {
+          createWindow();
+        },
+      },
+      {
+        label: "Clear All",
+      },
+      {
+        label: "Exit",
+        accelerator: process.platform === "darwin" ? "Command+Q" : "Ctrl+Q",
+        role: "quit",
+      },
+    ],
+  },
+];
+
+if (process.env.NODE_ENV !== "production") {
+  mainMenuTemplate.push({
+    label: "Dev Tools",
+    submenu: [
+      {
+        label: "Open Developer Window",
+        click(item, focusedWindow) {
+          focusedWindow.toggleDevTools();
+        },
+        accelerator:
+          process.platform === "darwin" ? "Command+Shift+I" : "Ctrl+Shift+I",
+      },
+      {
+        label: "Refresh",
+        role: "reload",
+      },
+    ],
+  });
+}
+
+function createWindow() {
+  addWindow = new BrowserWindow({
+    width: 482,
+    height: 200,
+    title: "New Window",
+    frame: false,
+  });
+
+  addWindow.setResizable(false);
+
+  addWindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname, "/pages/newTodo/newTodo.html"),
+      protocol: "file:",
+      slashes: true,
+    })
+  );
+
+  addWindow.on("close", () => {
+    addWindow = null;
+  });
+}
